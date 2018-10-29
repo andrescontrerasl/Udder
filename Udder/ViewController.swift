da25803aad4dac3ddb3b30b1b6e53c0fdc05f0d2
acontreraslama <andrescontreras@andress-macbook-pro-10.local>
2018-10-29 15:40:39 +1000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Andres Contreras on 29/10/18.
+//  Copyright © 2018 Andres Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
